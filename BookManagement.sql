c91309ac226438fda56951ce85de0aee110fcaec
Jen Liu <jkliu@vpn-18-101-16-185.mit.edu>
2013-04-20 16:42:27 +0800

    added checkout book functionality

---
+++ BookManagement.sql
@@ -0,0 +1,119 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.10.1deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Apr 11, 2013 at 12:41 AM
+-- Server version: 5.5.29
+-- PHP Version: 5.3.10-1ubuntu3.6
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `BookManagement`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE IF NOT EXISTS `admin` (
+  `admin_id` char(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `name` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `phone_no` char(11) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`admin_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book`
+--
+
+CREATE TABLE IF NOT EXISTS `book` (
+  `book_id` char(13) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `category` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `title` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `publisher` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `year` int(11) DEFAULT NULL,
+  `author` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `price` decimal(5,2) DEFAULT NULL,
+  `qty_total` int(11) DEFAULT NULL,
+  `qty_available` int(11) DEFAULT NULL,
+  PRIMARY KEY (`book_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Trigger to set the qty_available for each new book inputted
+--
+
+CREATE TRIGGER `set_qty_available` BEFORE INSERT ON `book`
+ FOR EACH ROW BEGIN
+SET NEW.qty_available = NEW.qty_total;
+END;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `borrow_record`
+--
+
+CREATE TABLE IF NOT EXISTS `borrow_record` (
+  `book_id` char(13) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `card_id` char(10) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `date_out` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `date_in` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `admin_id` char(10) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  KEY `book_id` (`book_id`),
+  KEY `card_id` (`card_id`),
+  KEY `admin_id` (`admin_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Trigger to update the qty_available when a book is checked out
+--
+
+CREATE TRIGGER `decrement_qty_available` AFTER INSERT ON `borrow_record`
+ FOR EACH ROW BEGIN
+UPDATE book SET book.qty_available = book.qty_available - 1 WHERE book.book_id = NEW.book_id;
+END;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `card`
+--
+
+CREATE TABLE IF NOT EXISTS `card` (
+  `card_id` char(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `department` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `card_type` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`card_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `borrow_record`
+--
+ALTER TABLE `borrow_record`
+  ADD CONSTRAINT `borrow_record_ibfk_1` FOREIGN KEY (`book_id`) REFERENCES `book` (`book_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `borrow_record_ibfk_2` FOREIGN KEY (`card_id`) REFERENCES `card` (`card_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_borrow_record_1` FOREIGN KEY (`admin_id`) REFERENCES `admin` (`admin_id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ checkout_book.php
@@ -0,0 +1,31 @@
+<?php
+
+include "./config_connect_database.php";
+
+$card_id = $_POST['card_id'];
+$book_id = $_POST['book_id'];
+$date_out = date("m/d/y");
+$admin_id = $_POST['admin_id'];
+
+// Check if there are enough books available
+$qty_avail = mysql_result(mysql_query("SELECT qty_available FROM book
+				WHERE book_id = '$book_id'"), 0);
+
+if($qty_avail > 0){
+	// create borrow_record
+	$record_qry = "INSERT INTO borrow_record
+					(book_id, card_id, date_out, admin_id)
+			  		VALUES
+			  		('$book_id', '$card_id', '$date_out', '$admin_id')";
+
+	$result = mysql_query($record_qry);
+	if($result){
+		echo("<br>Book successfully checked out.");
+	} else {
+		echo("Error");
+	}
+} else {
+	echo("There are not enough books available to be checked out at this time!");
+}
+
+?>
