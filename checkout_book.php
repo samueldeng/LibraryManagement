95da2d4bf344f74aba24408bc10f22d7e7847e90
Jen Liu <jkliu@vpn-18-101-16-226.mit.edu>
2013-04-21 21:34:29 +0800

    checkout book cleanup

--- checkout_book.php
+++ checkout_book.php
@@ -1,67 +1,74 @@
 <?php
+	include "./config_connect_database.php";
+	include "layout_top.php";
 
-include "./config_connect_database.php";
+	$card_id = $_POST['card_id'];
+	$book_id = $_POST['book_id'];
+	$date_out = date("m/d/y");
+	$admin_id = $_POST['admin_id'];
 
-$card_id = $_POST['card_id'];
-$book_id = $_POST['book_id'];
-$date_out = date("m/d/y");
-$admin_id = $_POST['admin_id'];
+	// Book checkout
+	if($book_id != '' and $admin_id != '') {
+		// Check if there are enough books available
+		$qty_avail = mysql_result(mysql_query("SELECT qty_available FROM book
+						WHERE book_id = '$book_id'"), 0);
 
-// Check if there are enough books available
-$qty_avail = mysql_result(mysql_query("SELECT qty_available FROM book
-				WHERE book_id = '$book_id'"), 0);
+		if($qty_avail > 0){
+			// create borrow_record
+			$borrow_qry = "INSERT INTO borrow_record
+							(book_id, card_id, date_out, admin_id)
+					  		VALUES
+					  		('$book_id', '$card_id', '$date_out', '$admin_id')";
 
-if($qty_avail > 0){
-	// create borrow_record
-	$borrow_qry = "INSERT INTO borrow_record
-					(book_id, card_id, date_out, admin_id)
-			  		VALUES
-			  		('$book_id', '$card_id', '$date_out', '$admin_id')";
+			$borrow_result = mysql_query($borrow_qry) or die ('Error: '.mysql_error ());
+			
+			if($borrow_result){
+				echo("<br>Book successfully checked out.");
 
-	$borrow_result = mysql_query($borrow_qry) or die ('Error: '.mysql_error ());
-	
-	if($borrow_result){
-		echo("<br>Book successfully checked out.");
+			} else {
+				echo("Error");
+			}
 
-	} else {
-		echo("Error");
+		} else {
+			echo("There are not enough books available to be checked out at this time!");
+		}
 	}
-
-} else {
-	echo("There are not enough books available to be checked out at this time!");
-}
-// Display books checked out by this user that haven't been returned yet.
-$result = $dbh->query("SELECT * from book
-						WHERE book_id in (SELECT book_id FROM borrow_record 
-											WHERE card_id = '$card_id'
-											AND date_in is NULL)");
+	// Display books checked out by this user that haven't been returned yet.
+	$result = $dbh->query("SELECT * from book WHERE book_id in 
+							(SELECT book_id 
+							FROM borrow_record 
+							WHERE card_id = '$card_id'
+							AND date_in is NULL)");
 ?>
 
-<table class="table table-striped">
-	<tr>
-		<th>ISBN</th>
-		<th>Category</th>
-		<th>Title</th>
-		<th>Published</th>
-		<th>Year</th>
-		<th>Author</th>
-		<th>Price</th>
-		<th>Owned</th>
-		<th>Available</th>
-	</tr>
-	<?php foreach(($result) as $row) : ?>
+<?php if(result) : ?>
+	<h3>Books currently checked out by Card <?php echo $card_id; ?></h3>
+	<table class="table table-striped">
 		<tr>
-			<td><?php echo $row['book_id']; ?></td>
-			<td><?php echo $row['category']; ?></td>
-			<td><?php echo $row['title']; ?></td>
-			<td><?php echo $row['publisher']; ?></td>
-			<td><?php echo $row['year']; ?></td>
-			<td><?php echo $row['author']; ?></td>
-			<td><?php echo $row['price']; ?></td>
-			<td><?php echo $row['qty_total']; ?></td>
-			<td><?php echo $row['qty_available']; ?></td>
+			<th>ISBN</th>
+			<th>Category</th>
+			<th>Title</th>
+			<th>Published</th>
+			<th>Year</th>
+			<th>Author</th>
+			<th>Price</th>
+			<th>Owned</th>
+			<th>Available</th>
 		</tr>
-	<?php endforeach ?>
-</table> 
-
-
+		<?php foreach(($result) as $row) : ?>
+			<tr>
+				<td><?php echo $row['book_id']; ?></td>
+				<td><?php echo $row['category']; ?></td>
+				<td><?php echo $row['title']; ?></td>
+				<td><?php echo $row['publisher']; ?></td>
+				<td><?php echo $row['year']; ?></td>
+				<td><?php echo $row['author']; ?></td>
+				<td><?php echo $row['price']; ?></td>
+				<td><?php echo $row['qty_total']; ?></td>
+				<td><?php echo $row['qty_available']; ?></td>
+			</tr>
+		<?php endforeach ?>
+	</table> 
+	
+<?php endif ?>
+<?php include "layout_bottom.php" ?>;
