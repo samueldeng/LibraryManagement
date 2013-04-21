51b5a0d07c027f6f10bbe98d866c8683192200f2
Samuel <mr.dengshuoling@gmail.com>
2013-04-21 20:54:04 +0800

    Bookquery.php bookqueryresult.php DONE

--- bookqueryresult.php
+++ bookqueryresult.php
@@ -1,17 +1,4 @@
 <?php
-/**
-*
-*	This Document is used to generate the HTML document to the bookquery.php in the bookquery Block
-*
-*
-*	( 书号, 类别, 书名, 出版社, 年份, 作者, 价格, 总藏书量, 库存 )
-*
-*	可选要求: 可以按用户指定属性对图书信息进行排序. (默认是书名)
-*
-*
-**/
-
-
 require_once('FirePHPCore/FirePHP.class.php');
 ob_start();
 $firephp = FirePHP::getInstance(true);
