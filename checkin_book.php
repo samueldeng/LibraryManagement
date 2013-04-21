c4fe0136d0ba36f683f20cfc9bb5ed8ac1d7f6c3
Samuel <mr.dengshuoling@gmail.com>
2013-04-22 01:11:53 +0800

    checkin.php DONE

--- checkin_book.php
+++ checkin_book.php
@@ -2,40 +2,57 @@
 
 include "./config_connect_database.php";
 include "./checklogin.php";
-
+ 
 $card_id = $_POST['card_id'];
 $book_id = $_POST['book_id'];
-$date_in = date("m/d/y");
 
-//Test Code Segment
-$firephp->log($card_id,"card_id");
-$firephp->log($book_id,"book_id");
-$firephp->log($date_in);
-//TODO
-//只输入card_id,输出借书的条目。
+$query = "SELECT * FROM borrow_record 
+             WHERE card_id='$card_id' and book_id='$book_id'";
 
 
-//还书
 
-//This Segment code was used in FirePHP
- require_once('FirePHPCore/FirePHP.class.php');
- ob_start();
- $firephp = FirePHP::getInstance(true);
-
-$borrow_record_query= mysql_query("SELECT * FROM borrow_record 
-             WHERE card_id='$card_id' and book_id='$book_id'");
+$borrow_record_query= mysql_query($query);
 $has_borrow_record = mysql_num_rows($borrow_record_query)==0?false:true;
 
 if($has_borrow_record){
-	$firephp->log($borrow_record_query,"borrow_record_query");
-	mysql_query("UPDATE book
-                 SET qty_available = qty_available + 1
-                 WHERE book_id= $book_id");//更新book.avalio
-	 msyql_query("Update borrow_record
-                 SET date_in = '$date_in'
-                 WHERE card_id='$card_id' and book_id='$book_id'");//更新归还时间
+
+	 mysql_query("UPDATE borrow_record
+                 SET date_in = CURDATE()
+                 WHERE card_id='$card_id' and book_id='$book_id'");
     	echo "checkin successfully";
-}  else {
-	echo "borrow record doesn't exit";
+
 }
+$result = $dbh->query("SELECT * from book
+			WHERE book_id in (SELECT book_id FROM borrow_record 
+			WHERE card_id = '$card_id'
+			AND date_in is NULL)");
+$firephp->log("query. finish");
 ?>
+
+
+<table class="table table-striped">
+	<tr>
+		<th>ISBN</th>
+		<th>Category</th>
+		<th>Title</th>
+		<th>Published</th>
+		<th>Year</th>
+		<th>Author</th>
+		<th>Price</th>
+		<th>Owned</th>
+		<th>Available</th>
+	</tr>
+<?php foreach(($result) as $row) : ?>
+        <tr>
+                <td><?php echo $row['book_id']; ?></td>
+                <td><?php echo $row['category']; ?></td>
+                <td><?php echo $row['title']; ?></td>
+                <td><?php echo $row['publisher']; ?></td>
+                <td><?php echo $row['year']; ?></td>
+                <td><?php echo $row['author']; ?></td>
+                <td><?php echo $row['price']; ?></td>
+                <td><?php echo $row['qty_total']; ?></td>
+                <td><?php echo $row['qty_available']; ?></td>
+        </tr>
+<?php endforeach ?>
+</table> 
