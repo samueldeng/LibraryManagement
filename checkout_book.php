bd09cc93e59c3f605e3f27d32887d691e763c787
Jen Liu <jkliu@Jens-MacBook-Pro.local>
2013-04-21 20:33:35 +0800

    separated layout code, added PDO connection to config, query for currently checked out books

--- checkout_book.php
+++ checkout_book.php
@@ -13,19 +13,55 @@ $qty_avail = mysql_result(mysql_query("SELECT qty_available FROM book
 
 if($qty_avail > 0){
 	// create borrow_record
-	$record_qry = "INSERT INTO borrow_record
+	$borrow_qry = "INSERT INTO borrow_record
 					(book_id, card_id, date_out, admin_id)
 			  		VALUES
 			  		('$book_id', '$card_id', '$date_out', '$admin_id')";
 
-	$result = mysql_query($record_qry);
-	if($result){
+	$borrow_result = mysql_query($borrow_qry) or die ('Error: '.mysql_error ());
+	
+	if($borrow_result){
 		echo("<br>Book successfully checked out.");
+
 	} else {
 		echo("Error");
 	}
+
 } else {
 	echo("There are not enough books available to be checked out at this time!");
 }
-
+// Display books checked out by this user that haven't been returned yet.
+$result = $dbh->query("SELECT * from book
+						WHERE book_id in (SELECT book_id FROM borrow_record 
+											WHERE card_id = '$card_id'
+											AND date_in is NULL)");
 ?>
+
+<table class="table table-striped">
+	<tr>
+		<th>ISBN</th>
+		<th>Category</th>
+		<th>Title</th>
+		<th>Published</th>
+		<th>Year</th>
+		<th>Author</th>
+		<th>Price</th>
+		<th>Owned</th>
+		<th>Available</th>
+	</tr>
+	<?php foreach(($result) as $row) : ?>
+		<tr>
+			<td><?php echo $row['book_id']; ?></td>
+			<td><?php echo $row['category']; ?></td>
+			<td><?php echo $row['title']; ?></td>
+			<td><?php echo $row['publisher']; ?></td>
+			<td><?php echo $row['year']; ?></td>
+			<td><?php echo $row['author']; ?></td>
+			<td><?php echo $row['price']; ?></td>
+			<td><?php echo $row['qty_total']; ?></td>
+			<td><?php echo $row['qty_available']; ?></td>
+		</tr>
+	<?php endforeach ?>
+</table> 
+
+
---
+++ layout_bottom.php
@@ -0,0 +1,20 @@
+	</div>
+		
+	<div id="footer">
+		<div class="container" align="center">
+			<p class="muted credit">
+			Created by
+			<a href="mailto:mr.dengshuoling@gmail.com?subject=Hello%20Shuoling!">@Shuoling Deng</a>
+			,
+			<a href="mailto:jenkliu@gmail.com?subject=Hello%20Jen!">@Jen Liu</a>
+			and
+			<a href="mailto:sdmorrisys@gmail.com?subject=Hello%20Shuo!">@Shuo Yang</a>
+			.
+			</p>
+		</div>
+	</div>
+	
+		<script src="http://code.jquery.com/jquery.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+	</body>
+</html>
