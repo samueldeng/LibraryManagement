aec644003aa6e5b4f0ba61e6526dbd6f8709b066
Shuo Yang <sdmorrisys@gmail.com>
2013-04-22 15:40:24 +0800

    separated navbar code on all pages, added checklogin.php

--- try_card_delete.php
+++ try_card_delete.php
@@ -85,84 +85,15 @@ margin: 20px 0;
 </style>
     <link rel="stylesheet" href="css/bootstrap-responsive.min.css">
 	<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
-	<script>
-		$(document).ready(function(){
-			$("#bookQueryForm").submit(function(event){
-				event.preventDefault();
-				$.post(
-					"./bookqueryresult.php",
-					{
-						category:$("#1").val(),
-						title:$("#2").val(), 
-						publisher:$("#3").val(),
-						year_min:$("#4").val(),    
-						year_max:$("#5").val(),
-						author:	$("#6").val(),
-						price_min:$("#7").val(),
-						price_max:$("#8").val()
-					},
-					function(data){
-						$("#bookQueryResultBlock").html(data);
-					}
-				);
 
-			});
-
-		});
-	</script> 
 	</head>
 	
 	
-	<body>
-		<div id="wrap">
-
-			<div class="navbar">
-				<div class="navbar-inner">
-					<a class="brand" href="try_card_delete.php">Librarymanage System</a>
-					<ul class="nav">
-						<li><a href="try_bookquery.php">Book_query</a></li>
-					</ul>
-					
-					<ul class="nav nav-tabs">
-						<li class="dropdown">
-							<a class="dropdown-toggle" data-toggle="dropdown" href="#">
-							Book_manage<b class="caret"></b>
-							</a>
-							<ul class="dropdown-menu">
-							<li><a href="try_bookborrow.php">Book_borrow</a></li>
-							<li><a href="try_bookreturn.php">Book_return</a></li>
-							
-							 <li class="dropdown-submenu">
-								 <a tabindex="-1" href="#">Book_entry</a>
-								 <ul class="dropdown-menu">
-								 <li><a href="try_bookentry_single.php">Book_entry_single</a></li>
-								 <li><a href="try_bookentry_multiple.php">Book_entry_multiple</a></li>
-								 </ul>
-							</li>
-							</ul>
-						</li>
-					</ul>
-
-					<ul class="nav nav-tabs">
-						<li class="dropdown">
-							<a class="dropdown-toggle" data-toggle="dropdown" href="#">
-							Card_manage<b class="caret"></b>
-							</a>
-							<ul class="dropdown-menu">
-							<li><a href="try_card_add.php">Card_add</a></li>
-							<li><a href="try_card_delete.php">Card_delete</a></li>
-							</ul>
-						</li>
-					</ul>
-								
-					<ul class="nav pull-right">
-						<li><a href="try.php">Exit</a></li>
-					</ul>
-					<p class="navbar-text" align="right">Hello, <a href="">xxxx</a>!<!--here maybe need a php transfered information-username--></p>
-				
-				</div>
-			</div>
-
+	<?php 
+include "./config_connect_database.php";
+include "checklogin.php";
+include "layout_top.php";
+?>
 		<div id="inputField" class="container">
 			
 			<form class="form-signin" method="post" action="delete_card.php">
