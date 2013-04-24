d708a34228f7dde8647b7ef2c1b0b63385f31200
Jen Liu <jkliu@mit.edu>
2013-04-24 21:32:47 +0800

    error message fixes, placement, styles

--- try_bookborrow.php
+++ try_bookborrow.php
@@ -84,35 +84,57 @@ max-width: 1100px;
 .container .credit {
 margin: 20px 0;
 }
+
+.alert {
+	min-width: 400px;
+	max-width: 100px;
+}
 </style>
     <link rel="stylesheet" href="css/bootstrap-responsive.min.css">
 	<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
 	</head>
 
 
-	<?php
-include "./config_connect_database.php";
-include "checklogin.php";
-include "layout_top.php";
+<?php
+	include "./config_connect_database.php";
+	include "checklogin.php";
+	include "layout_top.php";
+
+	if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+		$card_id = $_POST['card_id'];
+		$book_id = $_POST['book_id'];
+		$admin_id = $_POST['admin_id'];
+	}
 ?>
 
-<?php
-if($_SERVER["REQUEST_METHOD"] == "POST"){
 
-	$card_id = $_POST['card_id'];
-	$book_id = $_POST['book_id'];
-	$admin_id = $_POST['admin_id'];
+<div id="inputField" class="container">
+
+	<form class="form-signin" method="post" action="try_bookborrow.php">
+		<h2 align="center" class="form-signin-heading">Checking out</h2>
+		<label for="Card ID">Card ID</label><input type="text" name="card_id" value="<?php echo($card_id); ?>"/>
+		<label for="ISBN">ISBN</label><input type="text" name="book_id" value="<?php echo($book_id); ?>"/>
+		<label for="Admin ID">Admin ID</label><input type="text" name="admin_id" value="<?php echo($admin_id); ?>"/>
+		<!--Use present ID number?-->
+		<button class="btn btn-primary" type="submit">Checkout book</button>
+	</form>
+
+</div>
+
+<center>
+<?php
 
 	$card_qry = "SELECT COUNT(*) FROM card WHERE card_id = '$card_id'";
 	$book_qry = "SELECT COUNT(*) FROM book WHERE book_id = '$book_id'";
 	$admin_qry = "SELECT COUNT(*) FROM admin WHERE admin_id = '$admin_id'";
 
 	if($dbh->query($card_qry)->fetchColumn() == 0) {
-		echo("Please enter a valid card ID.");
+		echo("<div class='alert alert-error'>Please enter a valid card ID.</div>");
 	} else if($book_id != '' && $dbh->query($book_qry)->fetchColumn() == 0) {
-		echo("Please enter a valid ISBN.");
+		echo("<div class='alert alert-error'>Please enter a valid ISBN.</div>");
 	} else if($book_id != '' && $dbh->query($admin_qry)->fetchColumn() == 0) {
-		echo("Please enter a valid admin ID.");
+		echo("<div class='alert alert-error'>Please enter a valid admin ID.</div>");
 	} else {
 
 		// Book checkout
@@ -130,10 +152,10 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 			$borrow_result = mysql_query($borrow_qry) or die ('Error: '.mysql_error ());
 
 			if($borrow_result){
-				echo("<br>Book successfully checked out.");
+				echo("<div class='alert alert-success'>Book successfully checked out.</div>");
 
 			} else {
-				echo("Error");
+				echo("<div class='alert alert-error'>Error</div>");
 			}
 		} else {
 			// 且输出最近归还的时间
@@ -145,88 +167,84 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 				ORDER BY date_out ASC");
 			$next_avail = $sth->fetchColumn();
 			if($next_avail) {
-				echo("This book is not currently available. It should be available on ".$next_avail);
+				echo("<div class='alert alert-error'>This book is not currently available. It should be available on ".$next_avail."</div>");
 			}
 		}
 
-		// Get books checked out by this user that haven't been returned yet.
-		$result = $dbh->query(
-			"SELECT * from book
-			WHERE book_id in
-				(SELECT book_id
-				FROM borrow_record
-				WHERE card_id = '$card_id'
-				AND date_in is NULL)");	}
-}
+		$records_qry = "SELECT count(*) from book
+						WHERE book_id in
+							(SELECT book_id
+							FROM borrow_record
+							WHERE card_id = '$card_id'
+							AND date_in is NULL)";
+
+		if($dbh->query($records_qry)->fetchColumn() == 0) {
+			echo("<div class='alert alert-error'>There are no books checked out under this ID.</div>");
+		} else {
+
+			// Get books checked out by this user that haven't been returned yet.
+			$result = $dbh->query(
+				"SELECT * from book
+				WHERE book_id in
+					(SELECT book_id
+					FROM borrow_record
+					WHERE card_id = '$card_id'
+					AND date_in is NULL)");
+		}
+	}
+
 ?>
-
-
-		<div id="inputField" class="container">
-
-			<form class="form-signin" method="post" action="try_bookborrow.php">
-				<h2 align="center" class="form-signin-heading">Checking out</h2>
-				<label for="Card ID">Card ID</label><input type="text" name="card_id" value="<?php echo($card_id); ?>"/>
-				<label for="ISBN">ISBN</label><input type="text" name="book_id" value="<?php echo($book_id); ?>"/>
-				<label for="Admin ID">Admin ID</label><input type="text" name="admin_id" value="<?php echo($admin_id); ?>"/>
-				<!--Use present ID number?-->
-				<button class="btn btn-primary" type="submit">Checkout book</button>
-			</form>
-
-		</div>
-
-		<div id="push"></div>
-		</div>
 
 <p>
 	<?php if($result) : ?>
-		<?php if(count($result) > 0) : ?>
-			<?php //Display books checked out by this user that haven't been returned yet. ?>
-			<h3>Books currently checked out by Card <?php echo $card_id; ?></h3>
-			<table class="table table-striped">
+		<?php //Display books checked out by this user that haven't been returned yet. ?>
+		<h3>Books currently checked out by Card <?php echo $card_id; ?></h3>
+		<table class="table table-striped">
+			<tr>
+				<th>ISBN</th>
+				<th>Category</th>
+				<th>Title</th>
+				<th>Published</th>
+				<th>Year</th>
+				<th>Author</th>
+				<th>Price</th>
+				<th>Owned</th>
+				<th>Available</th>
+			</tr>
+			<?php foreach(($result) as $row) : ?>
 				<tr>
-					<th>ISBN</th>
-					<th>Category</th>
-					<th>Title</th>
-					<th>Published</th>
-					<th>Year</th>
-					<th>Author</th>
-					<th>Price</th>
-					<th>Owned</th>
-					<th>Available</th>
+					<td><?php echo $row['book_id']; ?></td>
+					<td><?php echo $row['category']; ?></td>
+					<td><?php echo $row['title']; ?></td>
+					<td><?php echo $row['publisher']; ?></td>
+					<td><?php echo $row['year']; ?></td>
+					<td><?php echo $row['author']; ?></td>
+					<td><?php echo $row['price']; ?></td>
+					<td><?php echo $row['qty_total']; ?></td>
+					<td><?php echo $row['qty_available']; ?></td>
 				</tr>
-				<?php foreach(($result) as $row) : ?>
-					<tr>
-						<td><?php echo $row['book_id']; ?></td>
-						<td><?php echo $row['category']; ?></td>
-						<td><?php echo $row['title']; ?></td>
-						<td><?php echo $row['publisher']; ?></td>
-						<td><?php echo $row['year']; ?></td>
-						<td><?php echo $row['author']; ?></td>
-						<td><?php echo $row['price']; ?></td>
-						<td><?php echo $row['qty_total']; ?></td>
-						<td><?php echo $row['qty_available']; ?></td>
-					</tr>
-				<?php endforeach ?>
-			</table>
-		<?php else : ?>
-			There are no books checked out under this ID.
-		<?php endif ?>
+			<?php endforeach ?>
+		</table>
 	<?php endif ?>
 </p>
+</center>
 
-	<div id="footer">
-		<div class="container" align="center">
-			<p class="muted credit">
-			Created by
-			<a href="mailto:mr.dengshuoling@gmail.com?subject=Hello%20Shuoling!">@Shuoling Deng</a>
-			,
-			<a href="mailto:jenkliu@gmail.com?subject=Hello%20Jen!">@Jen Liu</a>
-			and
-			<a href="mailto:sdmorrisys@gmail.com?subject=Hello%20Shuo!">@Shuo Yang</a>
-			.
-			</p>
-		</div>
+<div id="push"></div>
+</div>
+
+<div id="footer">
+	<div class="container" align="center">
+		<p class="muted credit">
+		Created by
+		<a href="mailto:mr.dengshuoling@gmail.com?subject=Hello%20Shuoling!">@Shuoling Deng</a>
+		,
+		<a href="mailto:jenkliu@gmail.com?subject=Hello%20Jen!">@Jen Liu</a>
+		and
+		<a href="mailto:sdmorrisys@gmail.com?subject=Hello%20Shuo!">@Shuo Yang</a>
+		.
+		</p>
 	</div>
+</div>
 
 
 		<script src="http://code.jquery.com/jquery.js"></script>
